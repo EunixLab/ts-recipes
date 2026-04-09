0dac9f2d97ba8ca517bc4d925cf44ab864d99121
Happy Ruda <01036156791@nate.com>
2026-04-09 14:06:36 +0900

    card 2차 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Detail from "@/pages/Detail";
 
+const isProd = import.meta.env.PROD;
+
 export default function App() {
   return (
     <Routes>
