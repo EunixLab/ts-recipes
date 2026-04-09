71016aceac8f458ea8251462a2f84e691f4a07b5
Happy Ruda <01036156791@nate.com>
2026-04-09 13:27:28 +0900

    card 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Detail from "@/pages/Detail";
-import "@/assets/scss/index.scss";
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -11,5 +10,3 @@ function App() {
     </Routes>
   );
 }
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `@use "@/assets/scss/abstracts" as *;`
-      },
-    },
-  },
+      }
+    }
+  }
 });
